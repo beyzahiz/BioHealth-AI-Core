227ec1b30e7ecd1e52412456f9587415067317f6
Beyza Hız <beyzahiz.03@gmail.com>
2026-05-06 14:59:27 +0300

    endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from app.services.mri_service import MRIService
+import os
 
 app = FastAPI(title = "BioHealth AI Core Platform")
+
+MODEL_PATH = "app/models/mri_model.keras" 
+mri_service = MRIService(MODEL_PATH)
 
 @app.get("/")
 def read_root():
@@ -8,5 +13,17 @@ def read_root():
 
 @app.get("/health")
 def health_check():
-    return {"status": "healthy", "services": ["mri", "nlp"]}
+    return {"status": "healthy", "services": ["mri"]}
+
+@app.post("/api/mri/analyze")
+async def analyze_mri(file: UploadFile = File(...)):
+    #kullanıcının gönderdiği fotoğrafı bytes olarak okur
+    image_bytes = await file.read()
+    
+    result = mri_service.predict(image_bytes)
+    
+    return {
+        "filename": file.filename,
+        "analysis": result
+    }
 
